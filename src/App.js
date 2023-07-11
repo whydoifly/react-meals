ea30a74042bc07c68bb006423548ece9625c1614
a.cheprasov <a.cheprasov@intermeda.ru>
2023-07-11 22:51:32 +0300

    Added Meals component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
-    <Header />
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+    
   );
 }
 
