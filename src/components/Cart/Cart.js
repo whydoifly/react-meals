562d7fdd07cbb91aea491ee73a099a13c53da088
a.cheprasov <a.cheprasov@intermeda.ru>
2023-07-13 23:36:27 +0300

    Added outputting of cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,37 @@
-import Modal from "../UI/Modal";
-import classes from "./Cart.module.css";
+import { useContext } from 'react';
+
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
+import CartContext from '../../context/cartContext';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const handleCartItemRemove = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const handleCartItemAdd = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id + Math.random()}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={handleCartItemRemove.bind(null, item.id)}
+          onAdd={handleCartItemAdd.bind(null, item)}
+          >
+          {item.name}
+        </CartItem>
       ))}
     </ul>
   );
@@ -14,11 +40,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
