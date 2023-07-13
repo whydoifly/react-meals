d844fd19887712fe5bc4f05d6d0b8cf4b8395f9a
a.cheprasov <a.cheprasov@intermeda.ru>
2023-07-13 17:48:02 +0300

    Added modal window state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { Fragment } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
+import { Fragment, useState } from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const handleShowCart = () => {
+    setCartIsShown(true);
+  };
+
+  const handleHideCart = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={handleHideCart} />}
+      <Header onShowCart={handleShowCart} />
       <main>
         <Meals />
       </main>
     </Fragment>
-    
   );
 }
 
