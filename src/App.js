05c0a9d275acd28ba32c0fb04e72523440896755
a.cheprasov <a.cheprasov@intermeda.ru>
2023-07-13 21:16:14 +0300

    Added a cart reducer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
-import CartProvider from './components/context/CartProvider';
+import CartProvider from './context/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
